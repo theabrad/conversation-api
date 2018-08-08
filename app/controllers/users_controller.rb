e725fb0c4ac856734c4cdb1f931c4be407bc7079
Alex Bradley <alex.robert.bradley@gmail.com>
2018-08-08 12:29:20 -0400

    Added knock for authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
